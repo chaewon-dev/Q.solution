fc203adb05c41c907931670b24a8dc7cb418dbd8
ch <chaewonim.dev@gmail.com>
2021-08-24 10:28:13 +0900

    3. 문장 속 단어  중 가장 긴 길이 찾기

---
+++ src/main/java/algorithm/problem/inflearnKimTaeWon/WordsInaSentence.java
@@ -0,0 +1,32 @@
+package algorithm.problem.inflearnKimTaeWon;
+
+import com.sun.source.tree.ReturnTree;
+
+import java.util.Scanner;
+
+public class WordsInaSentence {
+
+    public static String solution(String str) {
+
+        String[] strArr = str.split(" ");
+        int maxLen = 0;
+        int maxIndex = -1;
+
+        for (int i = 0; i < strArr.length; i++) {
+            int curLen = strArr[i].length();
+            if (maxLen < curLen) {
+                maxLen = curLen;
+                maxIndex = i;
+            }
+        }
+        String answer = new String(strArr[maxIndex]);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        System.out.println(solution(str));
+    }
+}
