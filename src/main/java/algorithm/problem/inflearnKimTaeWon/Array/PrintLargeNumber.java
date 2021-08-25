0ee7cf2d412348809729f4cad8ebc5020e0db16d
ch <chaewonim.dev@gmail.com>
2021-08-25 23:44:28 +0900

    2-1. 큰 수 출력하기

---
+++ src/main/java/algorithm/problem/inflearnKimTaeWon/Array/PrintLargeNumber.java
@@ -0,0 +1,35 @@
+package algorithm.problem.inflearnKimTaeWon.Array;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PrintLargeNumber {
+    public static ArrayList<Integer> solution(int n, int[] intArr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        answer.add(intArr[0]);
+        for (int i = 1; i < n; i++) {
+            if (intArr[i] > intArr[i - 1]) {
+                answer.add(intArr[i]);
+            }
+        }
+
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] intArr = new int[n];
+        for (int i = 0; i < n; i++) {
+            intArr[i] = sc.nextInt();
+        }
+
+        ArrayList<Integer> answer = solution(n, intArr);
+        for (Integer integer : answer) {
+            System.out.print(integer+" ");
+
+        }
+    }
+}
