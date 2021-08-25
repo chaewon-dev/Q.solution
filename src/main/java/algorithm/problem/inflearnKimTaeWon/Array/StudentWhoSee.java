6c4d75a944158f1d76d55438bdab574cd52c3eaa
ch <chaewonim.dev@gmail.com>
2021-08-26 04:34:11 +0900

    2-2. 보이는 학생

---
+++ src/main/java/algorithm/problem/inflearnKimTaeWon/Array/StudentWhoSee.java
@@ -0,0 +1,33 @@
+package algorithm.problem.inflearnKimTaeWon.Array;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class StudentWhoSee {
+
+    public static int solution(int n, int[] intArr) {
+        int answer = 1;
+        int maxIndex = 0;
+        for (int i = 1; i < n; i++) {
+            if (intArr[maxIndex] < intArr[i]) {
+                maxIndex = i;
+                answer++;
+            }
+        }
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] intArr = new int[n];
+        for (int i = 0; i < n; i++) {
+            intArr[i] = sc.nextInt();
+        }
+
+        System.out.println(solution(n,intArr));
+
+    }
+}
